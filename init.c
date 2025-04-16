46f681010ce74612c2d4ce6da5917018566cba91
tarik <66226638+ythknn@users.noreply.github.com>
2025-04-16 18:13:31 +0300

    init

--- init.c
+++ init.c
@@ -1,1 +1,18 @@
 #include "philosphers.h"
+
+
+
+void	init_args(int ac, char **av, t_data *data)
+{
+	int	i;
+
+	i = 0;
+	data->args.qty_of_philos = ft_atoi(av[++i]);
+	data->args.time_to_die = ft_atoi(av[++i]);
+	data->args.time_to_eat = ft_atoi(av[++i]);
+	data->args.time_to_sleep = ft_atoi(av[++i]);
+	if (ac == 5)
+		data->args.number_of_eat = FALSE;
+	else
+		data->args.number_of_eat = ft_atoi(av[++i]);
+}
